c3f8c2345368861e3afacd243b0c4403d59a0aa9
Jared Murray <jaredemurray@gmail.com>
2019-01-11 16:51:00 -0500

    Fixed copyright

--- lib/soldner_view_tool/renderer.rb
+++ lib/soldner_view_tool/renderer.rb
@@ -2,7 +2,7 @@ module SoldnerViewTool
   class Error < StandardError; end
   class Renderer
     def self.copyright(name, msg)
-      "&copyright; #{Time.now.year} | <b>#{name}</b> #{msg}".html_safe
+      "&copy; #{Time.now.year} | <b>#{name}</b> #{msg}".html_safe
     end
   end
 end
